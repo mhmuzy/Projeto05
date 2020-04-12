0610133601255be609cd8d44d58b720a38f5fe99
marcio henrique <mhmuzy@embratel.com.br>
2020-04-12 19:53:08 -0300

    Treinamento

---
+++ Projeto05/Abstracts/ProdutoRepositoryAbstract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Projeto05.Entities; //importando
+using Projeto05.Entities.Enums; //importando
+
+namespace Projeto05.Abstracts
+{
+    public abstract class ProdutoRepositoryAbstract
+    {
+        //declarando atributos na classe abstrata
+        //protected -> acesso por meio de herança
+
+    }
+}
