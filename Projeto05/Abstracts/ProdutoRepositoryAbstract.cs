f49eed4933eab8d5642c1e4d83a9facddff64a36
marcio henrique <mhmuzy@embratel.com.br>
2020-04-13 10:00:58 -0300

    Treinamento

--- Projeto05/Abstracts/ProdutoRepositoryAbstract.cs
+++ Projeto05/Abstracts/ProdutoRepositoryAbstract.cs
@@ -10,6 +10,31 @@ namespace Projeto05.Abstracts
     {
         //declarando atributos na classe abstrata
         //protected -> acesso por meio de herança
+        protected List<Produto> listagemDeProdutos;
 
+        //Método construtor
+        //utilizado quando uma classe é instanciada (new)
+
+        public ProdutoRepositoryAbstract()
+        {
+            //inicializando o atributo lista de produtos
+            listagemDeProdutos = new List<Produto>();
+        }
+
+        //métodos abstratos..
+
+        public abstract void Inserir(Produto produto);
+
+        public abstract void Atualizar(Produto produto);
+
+        public abstract void Excluir(Produto produto);
+
+        public abstract List<Produto> Consultar();
+
+        public abstract List<Produto> Consultar(string nome);
+
+        public abstract List<Produto> Consultar(decimal precoMin, decimal precoMax);
+
+        public abstract Produto ObterPorId(Guid idProduto);
     }
 }
