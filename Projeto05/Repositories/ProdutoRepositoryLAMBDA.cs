2cf0f337b0086b4977968209ddf1348c18384d37
marcio henrique <mhmuzy@embratel.com.br>
2020-04-13 22:00:52 -0300

    Treinamento

---
+++ Projeto05/Repositories/ProdutoRepositoryLAMBDA.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Projeto05.Entities;
+using Projeto05.Abstracts;
+
+namespace Projeto05.Repositories
+{
+    public class ProdutoRepositoryLAMBDA : ProdutoRepositoryAbstract
+    {
+
+        public override void Inserir(Produto produto)
+        {
+            listagemDeProdutos.Add(produto);
+        }
+
+        public override void Atualizar(Produto produto)
+        {
+            //buscar o produto dentro da lista atraves do id..
+            var item = listagemDeProdutos
+                        .Where(p => p.IdProduto == produto.IdProduto)
+                        .FirstOrDefault();
+
+            //verificar se algum produto foi encontrado
+            if (item != null)
+            {
+                item.Nome = produto.Nome;
+                item.Preco = produto.Preco;
+                item.Quantidade = produto.Quantidade;
+                item.Categoria = produto.Categoria;
+            }
+            else
+            {
+                throw new Exception("Erro. Produto não foi encontrado.");
+            }
+        }
+
+        public override void Excluir(Produto produto)
+        {
+            listagemDeProdutos.Remove(produto);
+        }
+
+        public override List<Produto> Consultar()
+        {
+            return listagemDeProdutos
+                    .OrderBy(p => p.Nome)
+                    .ToList();
+        }
+
+        public override List<Produto> Consultar(string nome)
+        {
+            return listagemDeProdutos
+                    .Where(p => p.Nome.Contains(nome))
+                    .OrderBy(p => p.Nome)
+                    .ToList();
+        }
+
+        public override List<Produto> Consultar(decimal precoMin, decimal precoMax)
+        {
+            return listagemDeProdutos
+                    .Where(p => p.Preco >= precoMin && p.Preco <= precoMax)
+                    .OrderByDescending(p => p.Preco)
+                    .ToList();
+        }
+
+        public override Produto ObterPorId(Guid idProduto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
